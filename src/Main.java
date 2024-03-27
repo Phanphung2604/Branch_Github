46a632aa11c9e39969025ab1dd75d22df336f13d
Phanphung2604 <minhphungphan26403@gmail.com>
2024-03-27 10:09:50 +0700

    Hello

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
-        for (int i = 1; i <= 5; i++) {
-
-            System.out.println("i = " + i);
-        }
+        Scanner scanner = new Scanner(System.in);
+        int a,b, tong, hieu, tich, thuong;
+        System.out.printf("Chương trinh tính toán cộng trừ nhân chia\n");
+        System.out.println("Nhập số a: ");
+        a = scanner.nextInt();
+        System.out.println("Nhập số b: ");
+        b = scanner.nextInt();
+        tong = a+b;
+        System.out.println("Phép cộng: " + tong);
+        hieu = a-b;
+        System.out.println("Phép trừ: " + hieu);
+        tich = a*b;
+        System.out.println("Phép nhân: " +tich);
+        thuong = a/b;
+        System.out.println("Phép chia: " +thuong);
     }
 }
